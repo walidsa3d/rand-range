e35a055855d6aefb62b707d31ed6ff6e5b83fa59
walid <walid.sa3d@gmail.com>
2015-11-02 14:37:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Random-Range
+Get a random number (integer or float) from a given range.
+
+#Install
+```
+$ npm install random-range
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+var randomInt = function(min, max) {
+    if (!isInteger(min) || !isInt(max))
+        throw Error;
+    if (min >= max)
+        throw Error;
+    else
+        return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var randomFloat = function(min, max) {
+    if (!isNumber(min) || !isNumber(max))
+        throw Error;
+    if (min >= max)
+        throw Error;
+    else
+        return Math.random() * (max - min) + min;
+};
+
+function isInteger(n) {
+    return Number(n) === n && n % 1 === 0;
+}
+
+function isFloat(n) {
+    return n === Number(n) && n % 1 !== 0;
+}
+
+function isNumber(n) {
+    return typeof n === 'number';
+}
+
+module.exports.randomInt = randomInt;
+module.exports.randomFloat = randomFloat;
